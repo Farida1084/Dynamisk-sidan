4acb5a8c05ba3df49f99672cddf56f2d1823bd32
Farida Ouro Möllah <farida1084@yahoo.fr>
2022-12-12 14:51:28 +0100

    Ändrat
    Komplettering.

--- cv.js
+++ cv.js
@@ -1,21 +1,20 @@
 //fetch
 
 const getInfo = async () => {
+  const request = new Request("./cv.json");
 
-    const request = new Request('./cv.json');
-    
-    const response = await fetch(request);
-    
-    const cvObj = await response.json();
-    
-    console.log(cvObj)
-    return cvObj;
-    }
-    
-    const populateCv = (cvObj) => {
-    let employmentHtml = ``;  
-    cvObj.employment.forEach(element => {
-        const listItem = `
+  const response = await fetch(request);
+
+  const cvObj = await response.json();
+
+  console.log(cvObj);
+  return cvObj;
+};
+
+const populateCv = (cvObj) => {
+  let employmentHtml = ``;
+  cvObj.employment.forEach((element) => {
+    const listItem = `
         <li>
         <h4>${element.time}</h4>
         <p>
@@ -24,54 +23,46 @@ const getInfo = async () => {
         <span>${element.text}</span>
         </p>
         </li>`;
-    
-        employmentHtml += listItem
-        console.log(cvObj.employment);
-    });
-    document.getElementById('employmentList').innerHTML = employmentHtml;
-    
-    let educationHtml = ``;
-    
-    cvObj.education.forEach(element => {
-        const listItem = `<li>${element}</li>`;
-    
-        educationHtml +=listItem
-        console.log(cvObj.education);
-    });
-    document.getElementById('educationList').innerHTML = educationHtml;
-    
-    let internshipsHtml = ``;
-    cvObj.internships.forEach(element => {
-        let listItem = `<li>
+
+    employmentHtml += listItem;
+    console.log(cvObj.employment);
+  });
+  document.getElementById("employmentList").innerHTML = employmentHtml;
+
+  let educationHtml = ``;
+
+  cvObj.education.forEach((element) => {
+    const listItem = `<li>${element}</li>`;
+
+    educationHtml += listItem;
+    console.log(cvObj.education);
+  });
+  document.getElementById("educationList").innerHTML = educationHtml;
+
+  let internshipsHtml = ``;
+  cvObj.internships.forEach((element) => {
+    let listItem = `<li>
         <p>
         <span >${element.companyName}</span>
         <span>${element.text}</span><br>
         </p>
         </li>`;
-        listItem = listItem.replace('#companyName',`<span>${element.companyName}</span>`)
-    
-        internshipsHtml +=listItem
-        console.log(cvObj.internships);
-    });
-    document.getElementById('internshipsList').innerHTML = internshipsHtml;
-    
-    
-    }
+    listItem = listItem.replace(
+      "#companyName",
+      `<span>${element.companyName}</span>`
+    );
 
-    const cvObj = await getInfo();
-    populateCv(cvObj);
+    internshipsHtml += listItem;
+    console.log(cvObj.internships);
+  });
+  document.getElementById("internshipsList").innerHTML = internshipsHtml;
+};
 
+const cvObj = await getInfo();
+populateCv(cvObj);
 
-    // Dynamisk delen
-    var btn = document.querySelector("button");
-    btn.onclick = () => {
-        window.print();
-
-    }
-
-
-
-
-   
-    
-    
+// Dynamisk delen
+var btn = document.querySelector("button");
+btn.onclick = () => {
+  window.print();
+};
